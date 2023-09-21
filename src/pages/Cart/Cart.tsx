92328cf9e7bae434495b6cb74227fe501c9cdfed
Bogdan <thegarrybit@gmail.com>
2023-09-21 19:45:02 +0300

    fix: cart adaptive

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -174,14 +174,15 @@ export default function Cart() {
           dark:text-primary
           lg:fixed
           lg:w-[200px]
-          lg:px-10
+          lg:px-4
           lg:py-28
           lg:pb-[210px]
           xl:w-[360px]
+          xl:px-10
       `
       }
     >
-      <h2 className="text-2xl font-medium">Your Order</h2>
+      <h2 className="text-xl font-medium xl:text-2xl">Your Order</h2>
       {isCartEmpty ? (
         <div className="m-auto grid justify-items-center self-center">
           <motion.img
@@ -197,9 +198,9 @@ export default function Cart() {
             initial={emptyCartInitial}
             animate={emptyCartAnimate}
             transition={{ ...emptyCartTransition, delay: 0.1 }}
-            className="mx-auto mt-5 w-fit text-center text-lg text-text-grey"
+            className="mx-auto mt-5 w-fit text-center text-text-grey xl:text-lg"
           >
-            <span className="text-3xl font-bold">Your cart is empty!</span> <br />
+            <span className="text-xl font-bold xl:text-3xl">Your cart is empty!</span> <br />
             <div className="mt-2">
               <span className="text-text-grey">Visit </span>
               <Link className="text-accent transition-all hover:text-accent/70" to="/">
